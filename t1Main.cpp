cc909a63e8948fdbc28873c96a470d4e54c4e62f
Camila <carivera14@espe.edu.ec>
2022-05-12 15:15:52 -0500

    Agrego indicaciones, creo mains

---
+++ t1Main.cpp
@@ -0,0 +1,20 @@
+/*ESPE
+*Enunciado del problema:
+*   A partir del codigo de clase, separar las funciones principales
+*   en clases.
+*Autor 
+*   Camila Rivera
+*   Mateo Roman
+*Fecha de creacion
+*   11-05-2022
+*Fecha de modificacion
+*    - 05 -2022
+*Grupo #2
+*Github del grupo:
+* https://github.com/Grupo8-4698-EstructuraDeDatos/4698-Grupo8-EstructuradeDatos
+*/
+
+int main(){
+    
+    return 0;
+}
---
+++ t2.Main.cpp
@@ -0,0 +1,21 @@
+/*ESPE
+*Enunciado del problema:
+*   Relizar la funcion ingreso de datos con sus excepciones,
+*   habilitar para flotantes, e implementar todo eso en clases
+*   abstractas a partir del codigo de clase
+*Autor 
+*   Camila Rivera
+*   Mateo Roman
+*Fecha de creacion
+*   11-05-2022
+*Fecha de modificacion
+*    - 05 -2022
+*Grupo #2
+*Github del grupo:
+* https://github.com/Grupo8-4698-EstructuraDeDatos/4698-Grupo8-EstructuradeDatos
+*/
+
+int main(){
+
+    return 0;
+}
