c37069cb2abca8a492d19dc50a18485bb1ce5373
Mateo <cmroman1@espe.edu.ec>
2022-05-13 01:04:25 -0500

    Se desarrollo el menu

--- Tarea3/t3Main.cpp
+++ Tarea3/t3Main.cpp
@@ -18,21 +18,46 @@
 #include <stdio.h>
 #include <conio.h>
 #include <string.h>
+#include <math.h>
 
 #include "t3IngresoDatos.cpp"
 
 int main(){
-    	IngresoDatos *a = new IngresoDatos();
-	char *dato=new char[50];
-	dato = a -> ingresoDatos("Ingrese sus numeritos: ");
-	
-	if((a -> getDet()) == '\0'){
-		printf("\n Su numero es %i, y es un entero", a -> devolverInt(dato));
-	}else if((a -> getDet()) == 'f'){
-		printf("\n Su numero es %f, y es un flotante", a -> devolverFloat(dato));
-	}else{
-		printf("\n No estoy funcionando");
-	}
+
+	int n, opcion;
+
+    do
+    {
+        printf( "\n-----Bienvenido escoja una opcion-----");
+		printf( "\n   1. Transformar el valor", 163 );
+        printf( "\n   2. Salir." );
+        printf( "\n\n   Introduzca opci%cn (1-2): ", 162 );
+
+        scanf( "%d", &opcion );
+
+        /* Inicio del anidamiento */
+
+        switch ( opcion )
+        {
+            case 1:     	
+				IngresoDatos *a = new IngresoDatos();
+				char *dato=new char[50];
+				dato = a -> ingresoDatos("Ingrese sus numeritos: ");
+				
+				if((a -> getDet()) == '\0'){
+					printf("\n Su numero es %i, y es un entero", a -> devolverInt(dato));
+				}else if((a -> getDet()) == 'f'){
+					printf("\n Su numero es %f, y es un flotante", a -> devolverFloat(dato));
+				}else{
+					printf("\n No estoy funcionando");
+				}
+                    break;
+         }
+
+         /* Fin del anidamiento */
+
+    } while ( opcion != 2 );
+
 
     return 0;
 }
