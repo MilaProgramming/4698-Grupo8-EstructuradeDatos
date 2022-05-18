f3baca79df39b38718f6c175011026d32abb326c
Camila Rivera <carivera14@>
2022-05-18 12:17:39 -0500

    actualizo git ignore

---
+++ Codigo.cpp
@@ -0,0 +1,25 @@
+//[11:10 AM] MSc. EDGAR FERNANDO SOLIS ACOSTA
+
+#include <iostream>
+#include <cstring>
+
+            void decimaBinario(int, char*);
+
+            void decimaBinario(int val, char *cad){
+                if(val >= 1){
+                    decimaBinario(val/2, cad);
+                    strcpy(cad,itoa((val%2),cad,10));
+                }
+                strcat(cad,"\0");
+                //printf("%s ", cad);
+            }
+
+int main(){
+
+    char *cad = new char[10];
+    decimaBinario(124, cad);
+
+    printf("%s ", cad);
+
+    return 0;
+}
