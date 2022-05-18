ca93443542e0d3cf9ee7f0a5be8e739eda493c61
Mateo <cmroman1@espe.edu.ec>
2022-05-18 00:19:36 -0500

    Operados llamada a funcion

---
+++ 18-05TareaDosGrupos/OperadorMateo/ListaPalabras.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>   // Para usar los manipuladores
+#include <string.h>
+using namespace std;
+
+struct elementos {
+    char *palabra;      // Palabra
+    int contador;       // Número de veces que aparece
+};
+// Interfaz, declaracion de la clase ListaPalabras
+
+class ListaPalabras {
+    private:
+        elementos *ptrLista;    // Puntero al comienzo de la lista de palabras
+        int nMaximo;            // Tamanio maximo de la lista
+        int indiceActual;       // Indice del primer elemento libre de la lista
+        friend class Acceso;
+    public:
+        ListaPalabras (int);
+        int &operator[] (char *);
+        void escribir_lista ();
+};
