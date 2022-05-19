84418f6733fbcf9a000577034450950988e0a5fb
Camila Rivera <carivera14@>
2022-05-19 18:08:54 -0500

    Retracto main en funcion solestructurada

--- SolEstructurada.cpp
+++ SolEstructurada.cpp
@@ -12,4 +12,15 @@ void decimaBinario(int val, char *cad){
     strcat(cad,"\0");
 }
 
+int main(){
+
+    char *cad = new char[10];
+    cad[0] = '\0';
+    decimaBinario(124, cad);
+
+    printf("%s ", cad);
+
+    return 0;
+}
+
 
