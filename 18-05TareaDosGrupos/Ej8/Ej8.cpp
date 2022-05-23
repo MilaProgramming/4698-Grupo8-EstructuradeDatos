4626c2de21f299618fb75be913bb423cbc0b4b2b
Mila <86137848+BeeMila@users.noreply.github.com>
2022-05-23 17:25:10 -0500

    Borro carpeta repetida.

--- 18-05TareaDosGrupos/Ej8/Ej8.cpp
+++
@@ -1,9 +0,0 @@
-#include "Ej8.h"
-
-Quebrado Quebrado::operator +(Quebrado &Q1, Quebrado &Q2)
-{    Quebrado Aux;
-     Aux.numerador = Q1.numerador * Q2.denominador +
-                     Q2.numerador * Q1.denominador;
-     Aux.denominador = Q1.denominador * Q2.denominador;
-     return Aux;
-}
