ceb80a6e52481914507b399615e919fee83f856e
Camila Rivera <carivera14@>
2022-06-06 13:54:05 -0500

    Agrego logicas de marquesina y proyecto

---
+++ Marquesina/Marquesina.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <ListaDC.hpp>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+
+    class Marquesina{
+        private:
+            string str;
+            ListaDobleC<T> lista;
+        public:    
+
+
+    };
---
+++ Marquesina/NodoDC.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+template <typename T>
+
+    class NodoDC{
+        private:
+            T valor;
+            NodoDC* anterior;
+            NodoDC* siguiente;
+        public:
+        
+            NodoDC(T v){
+                this->valor = v;
+            }
+
+            NodoDC(){}
+
+            ~NodoDC(){
+                delete anterior;
+                delete siguiente;
+            }
+
+            void setValor(T v){
+                this -> valor = v;
+            }
+
+            T getValor(){
+                return this->valor;
+            }
+
+            void setAnterior(NodoDC* ante){
+                this -> anterior = ante;
+            }
+
+            void setSiguiente(NodoDC* sig){
+                this -> siguiente = sig;
+            }
+
+            NodoDC* getAnterior(){
+                return this-> anterior;
+            }
+
+            NodoDC* getSiguiente(){
+                return this -> siguiente;
+            }
+
+    };
