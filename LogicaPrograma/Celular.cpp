85e3de7194707bd7fa7a0c1609b4f3aab23aa32e
Mateo <cmroman1@espe.edu.ec>
2022-06-07 00:43:42 -0500

    Gests y Sets Logica del programa

--- LogicaPrograma/Celular.cpp
+++ LogicaPrograma/Celular.cpp
@@ -4,6 +4,39 @@
 #include <string>
 using namespace std;
 
- Celular::Celular(){
+Celular::Celular(){
      
- }
+}
+ 
+ //Metodos
+ //Gests
+string Celular :: getMarca(){
+    return marca;
+}
+double Celular ::getPrecio(){
+    return precio;
+}
+int Celular ::getStock(){
+    return stock;
+}
+bool Celular ::getExistencia(){
+    return existe;
+}
+
+//Sets
+
+void Celular :: setMarca(string _marca){
+    marca = _marca;
+
+}
+void Celular ::setPrecio(double _precio){
+    precio = _precio;
+}
+void Celular ::setStock(int _stock){
+    stock = _stock;
+}
+void Celular ::setExistencia(bool _existe){
+    existe = _existe;
+}
+
+
--- LogicaPrograma/Persona.cpp
+++ LogicaPrograma/Persona.cpp
@@ -11,3 +11,31 @@ Persona::~Persona(){}
 void Persona::comprarCelular(){
     
 }
+ //Metodos
+ //Gests
+string Persona::getNombre(){
+    return nombre;
+
+}
+double Persona ::getPresupuesto(){
+    return presupuesto;
+}
+
+ListaDobleC<Celular>*  Persona ::getCelulares(){
+    return lista;
+}
+//Sets
+void Persona ::setNombre(string _nombre){
+    nombre = _nombre;
+
+}
+
+void Persona ::setPresupuesto(double _presupuesto){
+    presupuesto = _presupuesto;
+
+}
+void Persona :: setCelulares(ListaDobleC<Celular>*lista){
+    lista = lista;
+
+}
+
