2178a6ed2132135f456e46433bbde083fcdb21aa
Mateo <cmroman1@espe.edu.ec>
2022-06-07 21:15:19 -0500

    Menu 1

--- Main/main.cpp
+++ Main/main.cpp
@@ -18,11 +18,29 @@ using namespace std;
 int main(int argc, char **argv) {
    
     //!Creo objeto menu. Este manejara las opciones
-    Menu menu("Listas - menu");
+    Menu menu("---Bienvenidos---");
+
    
-    menu.add_option(MenuOption("1", [&](MenuOptionArguments args) {
+    menu.add_option(MenuOption("1. Stock", [&](MenuOptionArguments args) {
+        system("CLS");
+        //Submenu Stock
+        Menu menuStock("Submenu Stock");
+
+            menuStock.add_option(MenuOption("1. Ver Stock", [&](MenuOptionArguments args) {
+            }));
+            menuStock.add_option(MenuOption("2. Agregar Stock", [&](MenuOptionArguments args) {
+            }));
+            menuStock.add_option(MenuOption("3. Eliminar Stock", [&](MenuOptionArguments args) {
+            }));
+        
+            menuStock.add_option(MenuOption("\n4. Regresar a menu principal", [&](MenuOptionArguments args) {
+            menuStock.stop();
+            }, false));
+
+            //Mostrar menu
+            menuStock.display();
        
-        do {
+        /*do {
 
             //!Recibo datos de teclado. Me funciona para validar
             if (std::cin.fail()) {
@@ -32,35 +50,81 @@ int main(int argc, char **argv) {
             }
 
             break;
-        } while (true);
+        } while (true);*/
 
     }));
 
 
-    menu.add_option(MenuOption("2", [&](MenuOptionArguments args) {
+    menu.add_option(MenuOption("2. Usuarios ", [&](MenuOptionArguments args) {
         //!realizo una accion que no requiere input manual.
+        system("CLS");
+        //Submenu Usuarios
+        Menu menuUsuarios("Submenu Usuarios ");
+
+            menuUsuarios.add_option(MenuOption("1. Ver Usuarios", [&](MenuOptionArguments args) {
+            }));
+            menuUsuarios.add_option(MenuOption("2. Buscar Usuarios", [&](MenuOptionArguments args) {
+                system("CLS");
+                //Submenu Busqueda de Usuarios
+                Menu menuBusquedaUsuarios("Submenu Busqueda ");
+
+                    menuBusquedaUsuarios.add_option(MenuOption("1. Por Nombre", [&](MenuOptionArguments args) {
+                    }));
+                    menuBusquedaUsuarios.add_option(MenuOption("2. Por Presupuesto", [&](MenuOptionArguments args) {
+                    }));
+            }));        
+            menuUsuarios.add_option(MenuOption("\n3. Regresar a menu principal", [&](MenuOptionArguments args) {
+            menuUsuarios.stop();
+            }, false));
+
+            //Mostrar menu
+            menuUsuarios.display();
     }));
 
 
     //!Submenus
-    menu.add_option(MenuOption("3", [&](MenuOptionArguments args) {
+    menu.add_option(MenuOption("3. Comprar", [&](MenuOptionArguments args) {
         system("CLS");
-        Menu menuInterno("Submenu");
+        Menu menuComprar("Submenu Comprar");        
+            menuComprar.add_option(MenuOption("1...", [&](MenuOptionArguments args) {
+            }));
 
-        
-        menuInterno.add_option(MenuOption("1...", [&](MenuOptionArguments args) {
-        }));
-        
+            menuComprar.add_option(MenuOption("\n2. Regresar a menu principal", [&](MenuOptionArguments args) {
+            menuComprar.stop();
+            }, false));
 
-        menuInterno.add_option(MenuOption("\nRegresar a menu principal", [&](MenuOptionArguments args) {
-            menuInterno.stop();
-        }, false));
+        menuComprar.display();
+    }));
 
-        menuInterno.display();
+    menu.add_option(MenuOption("4. Recomendaciones", [&](MenuOptionArguments args) {
+        system("CLS");
+        //Submenu Recomendaciones 
+        Menu menuRecomendaciones("Submenu Recomendaciones");
+
+            menuRecomendaciones.add_option(MenuOption("1. Recomendar celulares para comprar", [&](MenuOptionArguments args) {
+                system("CLS");
+                //Submenu Recomendar celulares 
+                Menu menuReCelulares("Submenu Recomendar Celulares");
+
+                menuReCelulares.add_option(MenuOption("1. Recomendar automaticamente", [&](MenuOptionArguments args) {
+                }));
+                menuReCelulares.add_option(MenuOption("2. Recomendar por presupuesto", [&](MenuOptionArguments args) {
+                }));
+                menuReCelulares.add_option(MenuOption("\n3. Regresar a menu principal", [&](MenuOptionArguments args) {
+                menuReCelulares.stop();
+                }, false));
+
+                menuReCelulares.display();            
+            }));
+            menuRecomendaciones.add_option(MenuOption("\n2. Regresar a menu principal", [&](MenuOptionArguments args) {
+            menuRecomendaciones.stop();
+            }, false));
+
+        menuRecomendaciones.display();
     }));
 
     //!Funcion salida del menu
-    menu.add_option(MenuOption("salir", [&](MenuOptionArguments args) {
+    menu.add_option(MenuOption("5. salir", [&](MenuOptionArguments args) {
         cout << "\nGracias por usar el programa!" <<endl;
         menu.stop();
     }, false));
