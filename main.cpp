486377cc38cc081308ffa10bdfa425235e669ecf
Camila Rivera <carivera14@>
2022-08-14 17:01:57 -0500

    Final

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+/****************************
+*          UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+*
+* AUTOR(es):  CALAPAQUI GENESIS,JARA ARIEL,RIVERA CAMILA,ROMAN MATEO.
+* FECHA DE Inicio:  11 DE AGOSTO DE 2022
+* FECHA DE Entrega: 12 DE AGOSTO DE 2022
+* Version: final
+* ESTRUCTURA DE DATOS
+* NRC: 4698
+*****************************/
+
 #include <iostream>
 #include <windows.h>
 #include "color.hpp"
